18e1877a579e7dd0bcd4d5ac273184a2c156cd1c
kein <kein.you@gmail.com>
2017-01-21 03:25:11 +0900

    다음 scheme url 추가

--- app/src/main/java/kein/navihelper/CallExternalApp.java
+++ app/src/main/java/kein/navihelper/CallExternalApp.java
@@ -10,6 +10,8 @@ import android.net.Uri;
  * 외부 앱을 호출 한다.
  * 1. 다음지도 : 주소 검색창에 텍스트 입력
  * 2. 카카오 내비 : 길찾기 목적지 입력
+ *
+ * https://developers.daum.net/services/apis/urlscheme/reference#bm_us_daummaps
  */
 
 public class CallExternalApp {
